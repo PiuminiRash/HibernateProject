b3e21689b2a90647927d9d1593e0c4f1d2373dbc
PiuminiRash <piuminirashmika@gmail.com>
2024-01-26 10:12:05 +0530

    Initial commit

---
+++ src/main/java/lk/ijse/Main.java
@@ -0,0 +1,6 @@
+package lk.ijse;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/main/java/lk/ijse/entity/Student.java
@@ -0,0 +1,14 @@
+package lk.ijse.entity;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Student {
+    private int id;
+    private  String name;
+    private String address;
+}
